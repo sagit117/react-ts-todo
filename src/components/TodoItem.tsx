1e4f9c0b9c4a1846b3e3493438549cb3cec2d602
Павел Аксенов <sagit117@gmail.com>
2021-03-14 00:39:00 +0300

    start drag

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { ITodo } from "./TodoInput";
 
@@ -9,18 +9,29 @@ interface IProps {
 }
 
 function TodoItem(props: IProps) {
+    const [ drag, setDrag ] = useState<boolean>(false)
+
+    function dragHandler(v: boolean) {
+        setDrag(v)
+    }
+
     return (
-        <div className="todo-item mt-4 rounded">
-            <span className={ props.todo.complete ? 'completed': 'no-completed' }>{props.todo.title}</span>
+        <div
+            className={ `todo-item mt-4 rounded ${ drag ? 'drag' : '' }` }
+            draggable="true"
+            onDragStart={ () => dragHandler(true) }
+            onDragEnd={ () => dragHandler(false) }
+        >
+            <span className={ props.todo.complete ? 'completed': 'no-completed' }>{ props.todo.title }</span>
             <div className="todo-item-action">
                 <button
                     className="btn btn-outline-success mr-2"
-                    onClick={() => props.setStatus() }>
+                    onClick={ () => props.setStatus() }>
                     {props.todo.complete ? 'Выполнено': 'Не выполнено'}
                 </button>
                 <button
                     className="btn btn-danger"
-                    onClick={() => props.onRemoveTodo() }>
+                    onClick={ () => props.onRemoveTodo() }>
                     Удалить
                 </button>
             </div>
